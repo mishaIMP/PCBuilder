347b04441b99ed285a21dbefb569d8ea2827151d
mishaIMP <pshenichnicovmv@gmail.com>
2023-07-01 18:37:28 +0300

    update resources

--- bot/helper.py
+++
@@ -1,9 +0,0 @@
-def validate_price_range(pr: str) -> bool:
-    try:
-        min_price, max_price = map(int, pr.split('-'))
-        if min_price > max_price:
-            raise
-    except Exception as ex:
-        print(ex)
-        return False
-    return True
